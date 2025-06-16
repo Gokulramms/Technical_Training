416a09ab871f77e585e1cd94a12a79da0a2cad5e
Gokulramms <gokulramms@gmail.com>
2025-06-16 13:55:50 +0200

    Create octalorhexadecimaltodecimal.c

---
+++ octalorhexadecimaltodecimal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a1=0b0010;
+    int a2=0x12A6;
+    printf("This is Octal To Decimal:%d",a1);
+    printf("This is HexaDecimal To Decimal:%d",a2);
+    return 0;
+}
